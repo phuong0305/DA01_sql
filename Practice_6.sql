251b99e582f641a5b4b919b5bb3781ca01786b44
phuong0305 <155873530+phuong0305@users.noreply.github.com>
2024-01-17 00:59:06 +0700

    Practice_6.sql

---
+++ Practice_6.sql
@@ -0,0 +1,46 @@
+--EX1
+with job_id_count as
+(select company_id, COUNT(distinct job_id) FROM job_listings
+where title is not NULL and description is not NULL
+GROUP BY (company_id)
+HAVING COUNT(distinct job_id) >=2)
+select COUNT(distinct company_id) from Job_id_count;
+--EX2
+with total_spend_sum as
+(SELECT category, product, SUM(spend) as total_spend,
+RANK () OVER (partition by category ORDER BY sum(spend) DESC) as rank
+FROM product_spend
+where EXTRACT(year from transaction_date) = 2022
+group by category, product
+order by category, total_spend DESC)
+select category, product, total_spend
+from total_spend_sum
+where rank in (1,2);
+--EX3
+WITH count_call as
+(select policy_holder_id, COUNT(case_id) 
+FROM callers
+group by policy_holder_id
+having COUNT(case_id) >=3)
+SELECT COUNT(DISTINCT policy_holder_id) as member_count
+from count_call;
+--EX4
+select page_id from pages 
+where page_id not in (select page_id from page_likes)
+order by page_id;
+--EX5
+WITH month_7 as
+(SELECT distinct(user_id), event_type, EXTRACT(MONTH FROM event_date) as month FROM user_actions
+where EXTRACT(MONTH FROM event_date) = 7 AND 
+EXTRACT(year FROM event_date) = 2022 AND
+event_type in ('sign_in', 'like','comment')),
+month_6 as
+(SELECT distinct(user_id), event_type FROM user_actions
+where EXTRACT(MONTH FROM event_date) = 6 AND 
+EXTRACT(year FROM event_date) = 2022 AND
+event_type in ('sign_in', 'like','comment'))
+SELECT month_7.month as month, COUNT(distinct month_7.user_id) as monthly_active_users
+from month_7
+join month_6
+ON month_7.user_id = month_6.user_id
+group by month_7.month;
